c295cb0559b19f7bfa1b538f4e088908c01d8344
Ruta Reisoglu <rutareiso@protonmail.com>
2021-12-02 21:58:27 +0000

    Refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,14 +4,16 @@ const searchURL = "https://api.thecatapi.com/v1/breeds/search?q=";
 
 const fetchBreedDescription = function(breedName, callback) {
   request(`${searchURL}${breedName}`, (error, response) => {
-    let descriptions = {};
     if (!error){
+      let descriptions = {};
       const cats = JSON.parse(response.body);
       for (let cat of cats) {
         descriptions[cat.name] = cat.description
       }
+      callback(null, descriptions);
+    }else {
+      callback(error, null);
     }
-    callback(error, descriptions);
   });
 };
 
